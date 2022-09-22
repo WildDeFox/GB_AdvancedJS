7b9cb1722cda2c73b62f465456cbf40cc617cad9
DeFox <wildmrdefox@gmail.com>
2022-09-22 15:15:08 +0700

    Lesson_4

---
+++ lesson_4/script.js
@@ -0,0 +1,44 @@
+//ToDo - вам необходимо скопировать данный скрипт в свой проект, и сдавать ДЗ в рамках собственного репозитория
+
+
+// 1) Какие виды областей видимости вы знаете? Написать ответ ниже
+// - Голбальная область видимости
+// - Локальная область видимости
+// - Блочная область видимости
+// - Область видимости try/catch
+// - Область видимости eval
+
+// 2) Исправьте код так чтобы в консоль выводились числа от 0 до 10
+for (i = 0; i <= 10; i++) {
+  (function(i){
+    setTimeout(() => {
+      console.log(i);
+   }, 0);
+  })(i);
+}
+
+
+// 3) Исправьте код так чтобы в консоль выводилось "John"
+var firstName = "Elena"
+const obj = {
+  firstName: 'John',
+  sayFirstName(){
+     console.log(this.firstName)
+  }
+}
+obj.sayFirstName();
+
+
+// 4) Исправьте код так чтобы в консоль не выводилась ошибка (нельзя исправлять тело функции getArrowFunction)
+const user = {
+  age: 20
+}
+function getArrowFunction() {
+  "use strict"
+  return () => {
+     console.log(this.age)
+  }
+}
+const arrowFunction = getArrowFunction.bind(user);
+arrowFunction()();
+debugger
